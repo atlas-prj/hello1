e2a169ed972adc7010e7b6bb666bb2a20a65eec6
atlas <atlas@localhost>
2020-05-10 19:06:01 +0300

    test

--- src/main/java/ru/atlas/hello1/PrimaryController.java
+++ src/main/java/ru/atlas/hello1/PrimaryController.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import javafx.fxml.FXML;
 
 public class PrimaryController {
-
+// test
     @FXML
     private void switchToSecondary() throws IOException {
         App.setRoot("secondary");
